03aef28bbe1b797bfefa1371ab5c6d76a05c69cc
Andrew Elsegood <andy.elsegood@hotmail.com>
2020-05-06 16:24:02 +1000

    adding vscode config for easy debugging

--- tableDraw.py
+++ tableDraw.py
@@ -1,1 +1,10 @@
 
+from PyQt5 import QtGui,QtCore,QtWidgets
+from PyQt5.QtCore import QDateTime, Qt, QTimer,QCoreApplication
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import (QApplication, QGridLayout, QGroupBox, QComboBox,
+                             QHBoxLayout, QLabel, QLineEdit, QPushButton,
+                             QSizePolicy, QDialog, QTableWidget, QTextEdit,
+                             QVBoxLayout, QWidget, QFileDialog, QTableWidgetItem)
+
+print("No Problem ImportingLibraries --> Starting the tool -->")
